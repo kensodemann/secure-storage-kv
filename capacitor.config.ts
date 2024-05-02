40dd8b4080f657c5318b12a616a76502fdc0077b
Ken Sodemann <ken@ionic.io>
2024-05-02 11:48:53 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'secure-storage-kv',
+  webDir: 'www',
+};
+
+export default config;
